736165638dab3cb145d4b553915e90b2642c93cf
nmandrus1 <nmandrus1@gmail.com>
2023-03-07 21:46:35 -0500

    basic wayland apps updated

---
+++ .config/rofi/powermenu.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+ 
+lock="  Lock"
+logout="  Logout"
+shutdown="襤 Poweroff"
+reboot="  Reboot"
+sleep="  Suspend"
+ 
+selected_option=$(echo "$lock
+$logout
+$sleep
+$reboot
+$shutdown" | rofi -dmenu -i -p "Powermenu" \
+		  -theme "~/.config/rofi/powermenu.rasi")
+
+if [ "$selected_option" = "$lock" ]; then
+  swaylock
+elif [ "$selected_option" = "$logout" ]; then
+  loginctl terminate-user $USER
+elif [ "$selected_option" = "$shutdown" ]; then
+  systemctl poweroff
+elif [ "$selected_option" = "$reboot" ]; then
+  systemctl reboot
+elif [ "$selected_option" = "$sleep" ]; then
+  systemctl suspend
+else
+  echo "No match"
+fi
